670b288c142bace673c89676c7aeacabe882f179
stembrino <stembrino@gmail.com>
2024-01-24 22:46:50 +0000

    apply filter: TeamOverview page

--- src/pages/TeamOverview.tsx
+++ src/pages/TeamOverview.tsx
@@ -1,71 +1,72 @@
 import React from 'react';
 import {useLocation} from 'react-router-dom';
-import {ListItem, UserData} from 'types';
+import {ListItem} from 'types';
+import SearchBar from 'components/SearchBar';
 import Card from '../components/Card';
 import {Container} from '../components/GlobalComponents';
 import Header from '../components/Header';
 import List from '../components/List';
 import {useTeamUsers} from './hooks/useTeamUsers';
 
-var mapArray = (users: UserData[]) => {
-    return users.map(u => {
-        var columns = [
-            {
-                key: 'Name',
-                value: `${u.firstName} ${u.lastName}`,
-            },
-            {
-                key: 'Display Name',
-                value: u.displayName,
-            },
-            {
-                key: 'Location',
-                value: u.location,
-            },
-        ];
-        return {
-            id: u.id,
-            url: `/user/${u.id}`,
-            columns,
-            navigationProps: u,
-        };
-    }) as ListItem[];
-};
-
-
 const TeamOverview: React.FC = () => {
     const location = useLocation();
-    const {isLoading, teamUsers: {teamLead, teamMembers}} = useTeamUsers();
+    const {isLoading, teamLead, teamMembersFiltered, query, setQuery} = useTeamUsers();
 
     const teamLeadCard = (): JSX.Element | null => {
         if(!teamLead) {return null;}
         const columnsConfig = [
-        {
-            key: 'Team Lead',
-            value: '',
-        },
-        {
-            key: 'Name',
-            value: `${teamLead.firstName} ${teamLead.lastName}`,
-        },
-        {
-            key: 'Display Name',
-            value: teamLead.displayName,
-        },
-        {
-            key: 'Location',
-            value: teamLead.location,
-        },
-    ];
+            {
+                key: 'Team Lead',
+                value: '',
+            },
+            {
+                key: 'Name',
+                value: `${teamLead.firstName} ${teamLead.lastName}`,
+            },
+            {
+                key: 'Display Name',
+                value: teamLead.displayName,
+            },
+            {
+                key: 'Location',
+                value: teamLead.location,
+            },
+        ];
 
-    return <Card columns={columnsConfig} url={`/user/${teamLead.id}`} navigationProps={teamLead} />;
-};
+        return <Card columns={columnsConfig} url={`/user/${teamLead.id}`} navigationProps={teamLead} />;
+    };
+
+    const teamMembers = () => {
+        return teamMembersFiltered.map(u => {
+            var columns = [
+                {
+                    key: 'Name',
+                    value: `${u.firstName} ${u.lastName}`,
+                },
+                {
+                    key: 'Display Name',
+                    value: u.displayName,
+                },
+                {
+                    key: 'Location',
+                    value: u.location,
+                },
+            ];
+            return {
+                id: u.id,
+                url: `/user/${u.id}`,
+                columns,
+                navigationProps: u,
+            };
+        }) as ListItem[];
+    };
 
     return (
         <Container>
             <Header title={`Team ${location.state.name}`} />
+            <SearchBar onChange={setQuery} value={query} placeholder='Search team member'  width={320}/>
             {!isLoading && teamLeadCard()}
-            <List items={mapArray(teamMembers ?? [])} isLoading={isLoading} />
+            <List items={teamMembers()} isLoading={isLoading} />
         </Container>
     );
 };
--- src/pages/hooks/useTeamUsers.ts
+++ src/pages/hooks/useTeamUsers.ts
@@ -1,32 +1,39 @@
 import {getTeamOverview, getUserData} from 'api';
-import {useEffect, useState} from 'react';
+import {useEffect, useMemo, useState} from 'react';
 import {useParams} from 'react-router-dom';
 import {UserData} from 'types';
 
-interface TeamUsers {
-    teamLead?: UserData;
-    teamMembers?: UserData[];
-}
-
 export const useTeamUsers = () => {
     const {teamId} = useParams();
-    const [teamUsers, setTeamUsers] = useState<TeamUsers>({});
+    const [teamLead, setTeamLead] = useState<UserData>({} as UserData);
+    const [teamMembers, setTeamMembers] = useState<UserData[]>([]);
     const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [query, setQuery] = useState('');
 
     useEffect(() => {
         const getTeamUsers = async () => {
             const {teamLeadId, teamMemberIds = []} = await getTeamOverview(teamId);
-            const [teamLead, ...teamMembers] = await Promise.all([getUserData(teamLeadId), ...teamMemberIds.map(getUserData)]);
+            const [teamLeadResponse, ...teamMembersResponse] = await Promise.all([getUserData(teamLeadId), ...teamMemberIds.map(getUserData)]);
             setIsLoading(() => {
-                setTeamUsers({teamLead, teamMembers});
+                setTeamLead(teamLeadResponse);
+                setTeamMembers(teamMembersResponse);
                 return false;
             });
         };
         getTeamUsers();
     }, [teamId]);
 
+    const teamMembersFiltered = useMemo(() => {
+        return teamMembers.filter(({firstName, lastName}) => {
+            const fullName = `${firstName} ${lastName}`;
+            return fullName.replace(/\s/g, '').toLowerCase().includes(query.replace(/\s/g, '').toLowerCase());});
+    }, [teamMembers, query]);
+
     return {
-        teamUsers,
+        teamLead,
         isLoading,
+        teamMembersFiltered,
+        setQuery,
+        query,
     };
 };
