9bb14f0207ed951852b53688a399fa634cc66918
stembrino <stembrino@gmail.com>
2024-01-25 17:41:48 +0000

    add unit test "render spinner while loading"
    - improve the code quality to test

--- src/pages/__tests__/testTeams.tsx
+++ src/pages/__tests__/testTeams.tsx
@@ -16,36 +16,31 @@ jest.mock('react-router-dom', () => ({
 }));
 
 describe('Teams', () => {
-    beforeAll(() => {
-        jest.useFakeTimers();
-    });
-
-    afterEach(() => {
-        jest.clearAllTimers();
-    });
-
-    afterAll(() => {
-        jest.useRealTimers();
-    });
-
-    it('should render spinner while loading', async () => {
-        // TODO - Add code for this test
-    });
-
-    it('should render teams list', async () => {
-        jest.spyOn(API, 'getTeams').mockResolvedValue([
-            {
-                id: '1',
-                name: 'Team1',
-            },
-            {
-                id: '2',
-                name: 'Team2',
-            },
-        ]);
-
+    const getTeamsMocked = [
+        {
+            id: '1',
+            name: 'Team1',
+        },
+        {
+            id: '2',
+            name: 'Team2',
+        },
+    ];
+    const setUp = () => {
+        jest.spyOn(API, 'getTeams').mockResolvedValue(getTeamsMocked);
         render(<Teams />);
-
+    };
+    
+    it('should render spinner while loading', async () => {
+        setUp();
+        expect(screen.getByTestId('spinner')).toBeInTheDocument();
+        await waitFor(() => {
+            expect(screen.queryByTestId('spinner')).not.toBeInTheDocument();
+        });
+    });
+    
+    it('should render teams list', async () => {
+        setUp();
         await waitFor(() => {
             expect(screen.getByText('Team1')).toBeInTheDocument();
         });
